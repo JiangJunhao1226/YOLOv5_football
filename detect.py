9eadcf5eab6263c27c57c876724b2b01d9f45671
UltralyticsAssistant <web@ultralytics.com>
2026-07-25 04:03:24 +0000

    Auto-format by https://ultralytics.com/actions

--- detect.py
+++ detect.py
@@ -365,8 +365,16 @@ def parse_opt():
         ```
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", nargs="+", type=str, default=ROOT / r"D:\YOLOV5\yolov5-master\runs\train\exp5\weights\best.pt", help="model path or triton URL")
-    parser.add_argument("--source", type=str, default=ROOT / "datasets/test/images", help="file/dir/URL/glob/screen/0(webcam)")
+    parser.add_argument(
+        "--weights",
+        nargs="+",
+        type=str,
+        default=ROOT / r"D:\YOLOV5\yolov5-master\runs\train\exp5\weights\best.pt",
+        help="model path or triton URL",
+    )
+    parser.add_argument(
+        "--source", type=str, default=ROOT / "datasets/test/images", help="file/dir/URL/glob/screen/0(webcam)"
+    )
     parser.add_argument("--data", type=str, default=ROOT / "data/football.yaml", help="(optional) dataset.yaml path")
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[640], help="inference size h,w")
     parser.add_argument("--conf-thres", type=float, default=0.25, help="confidence threshold")
